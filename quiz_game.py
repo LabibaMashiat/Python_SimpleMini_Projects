7bb3fe932ee939d9200ef353f211517528d05c28
LabibaMashiat20 <labibamashiatmahi@gmail.com>
2024-11-16 21:03:58 +0600

    New commit message

--- quiz_game.py
+++ quiz_game.py
@@ -6,9 +6,30 @@ if playing!="yes":
 else:
     print("Okay! Let's play :) ")  
 score=0      
-answer=input("What does CPU stand for?").lower()
-if answer=="central processing key":
+answer=input("What does CPU stand for? ").lower()
+if answer=="central processing unit":
     print("Correct!")   
-    score+-1
+    score+=1
 else:
     print("Incorrect!")   
+answer=input("What does GPU stand for? ").lower()
+if answer=="graphic processing unit":
+    print("Correct!")   
+    score+=1
+else:
+    print("Incorrect!")   
+answer=input("What does RAM stand for? ").lower()
+if answer=="random access memory":
+    print("Correct!")   
+    score+=1
+else:
+    print("Incorrect!")   
+answer=input("What does PSU stand for? ").lower()
+if answer=="power supply":
+    print("Correct!")   
+    score+=1
+else:
+    print("Incorrect!")   
+print("You have  got "+ str(score )+" questions correct.")  
+print("You have got "+ str((score/4)*100) +" % "+"in the quiz.")    
+
